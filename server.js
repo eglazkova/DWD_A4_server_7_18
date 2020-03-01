1b3c230de008d66cfc5e8df437aafd6dc8c397ca
eglazkova <54900481+eglazkova@users.noreply.github.com>
2020-02-29 22:03:08 -0500

    challenge_9, questionable

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const fs = require("fs");
+
+const contents = fs.readFileSync(path.join(__dirname, "./data/whatCanIDo.json"));
+const skills = JSON.parse(contents);
 
 app.use(express.static("public"));
 
@@ -10,6 +14,10 @@ app.get("/", (req, res) => {
 
 app.get("/about", (req, res) => {
   res.sendFile(path.join(__dirname, "views/about.html"));
+});
+
+app.get("/skills", (req, res) => {
+  res.json(skills);
 });
 
 app.listen(3000, () => {
